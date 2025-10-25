9b347b97d425cf610cd9937bc6aab47a3e4b57c2
Denis Sanchez <deslvvpr@gmail.com>
2025-10-24 20:00:33 -0500

    fix: CORS in change-languaje and check-status js

--- api/change-language.js
+++ api/change-language.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   // Configurar CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
   // Manejar preflight requests
